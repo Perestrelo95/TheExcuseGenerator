cc8c6e2799865fdd124d168d3ef92b536f91c786
Perestrelo95 <84213212+Perestrelo95@users.noreply.github.com>
2022-05-09 22:49:36 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () =>
+  //cuando termina de cargar la pag carga una funcion anonima ()
+  {
+    let personaje = ["El gato", "El perro", "El chivo", "El pez", "El loro"];
+    let accion = ["enterro mi tarea","rompio mi cuaderno","orino mi lapiz","ensucicio mi camisa","cerro la puerta"];
+    let tiempo = ["luego de la clase","justo ahora","hace un rato","junto antes de la clase","cuando llegue"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    var person1 = (document.querySelector("#mensaje1").innerHTML =
+      personaje[Math.floor(Math.random() * personaje.length)]);
+    var accion1 = (document.querySelector("#mensaje2").innerHTML =
+      accion[Math.floor(Math.random() * accion.length)]);
+    var tiempo1 = (document.querySelector("#mensaje3").innerHTML =
+      tiempo[Math.floor(Math.random() * tiempo.length)]);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    console.log(person1);
+    console.log(accion1);
+    console.log(tiempo1);
+  };
